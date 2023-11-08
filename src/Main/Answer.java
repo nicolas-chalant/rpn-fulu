446bc6402254a1ede9758ca9d7d29aca17ed4acf
Joel <joelgabbai@gmail.com>
2023-11-08 10:24:22 +0100

    First version of the function (no tests)

--- src/Main/Answer.java
+++ src/Main/Answer.java
@@ -1,8 +1,36 @@
 package src.Main;
 
+import java.util.ArrayList;
+
 public class Answer {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("2");
+        arr.add("8");
+        arr.add("*");
+        System.out.println(computeRPN(arr));
+    }
+
+    public static int computeRPN(ArrayList<String> arr){
+        int result = 0;
+        ArrayList<Integer> stack = new ArrayList<Integer>();
+        stack.add(Integer.parseInt(arr.get(0)));
+        stack.add(Integer.parseInt(arr.get(1)));
+        arr.remove(0);
+        arr.remove(0);
+        
+        String operator = arr.get(0);
+        if(operator.equals("+")){
+            result = stack.get(0) + stack.get(1);
+        }else if(operator.equals("-")){
+            result = stack.get(0) - stack.get(1);
+        }else if(operator.equals("*")){
+            result = stack.get(0) * stack.get(1);
+        }else if(operator.equals("/")){
+            result = stack.get(0) / stack.get(1);
+        }
+
+        return result;
     }
 
 }
